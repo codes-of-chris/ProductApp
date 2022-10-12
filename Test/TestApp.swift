d15460619b310a65963d43ecb200b33eadf08cf1
Christopher Andrews <chris.andrews@meater.com>
2022-10-12 19:50:57 +0100

    Initial commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Chris Andrews on 12/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
